cc3c00cfc937fad43fe662517c11084ec2a46a78
Bhargav Sri Ram <kasukurthibhargav@gmail.com>
2025-11-08 01:45:51 +0530

    Remove frontend submodule

--- frontend/src/api.js
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-const API = axios.create({ baseURL: "http://localhost:5000/api" });
-
-export const getProfiles = () => API.get("/profiles");
-export const createProfile = (data) => API.post("/users", data);
-export const getEvents = (profileId) => API.get(`/events/${profileId}`);
-export const createEvent = (data) => API.post("/events", data);
-export const updateEvent = (id, data) => API.put(`/events/${id}`, data);
